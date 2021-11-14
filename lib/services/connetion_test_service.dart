efaa4cb99ea17e38d1023e51630202a8b3eccbf2
cezariusz2 <cezarypalewicz@gmail.com>
2021-11-14 21:34:12 +0100

    corrected url

--- lib/services/connetion_test_service.dart
+++ lib/services/connetion_test_service.dart
@@ -1,7 +1,7 @@
 import 'package:http/http.dart' as http;
 
 class ConnectionTestService {
-  var url = Uri.parse('127.0.0.1');
+  var url = Uri.parse('http://10.0.2.2:8000'); // ustawić dla swojego urządzenia
   Future testConnectionToTheServer() async {
     var resp = await http.get(url);
     if (resp.statusCode == 200) {
