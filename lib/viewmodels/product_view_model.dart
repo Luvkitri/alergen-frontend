304e9194e58c51f7631c47827d460dda63a8b910
kombajno678 <kombajno678@gmail.com>
2021-12-13 10:34:16 +0100

    smarter string comparison between users allergy and products allergen

--- lib/viewmodels/product_view_model.dart
+++ lib/viewmodels/product_view_model.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:frontend/locator.dart';
 import 'package:frontend/models/allergy_model.dart';
 import 'package:frontend/models/product_model.dart';
@@ -19,6 +20,60 @@ class ProductViewModel extends BaseModel {
   }
 
   bool userHasAllergy(String allergy) {
-    return userAllergies.where((element) => element.name == allergy).isNotEmpty;
+    return userAllergies
+        .where((element) => compareAllergyNames(element.name, allergy))
+        .isNotEmpty;
+  }
+
+  bool compareAllergyNames(String s1, String s2) {
+    return similarity(s1, s2) >= 0.9;
+  }
+
+  double similarity(String a, String b) {
+    double _similarity;
+    a = a.toUpperCase();
+    b = b.toUpperCase();
+    _similarity = 1 - levenshtein(a, b) / (max(a.length, b.length));
+    return (_similarity);
+  }
+
+  int levenshtein(String a, String b) {
+    a = a.toUpperCase();
+    b = b.toUpperCase();
+    int sa = a.length;
+    int sb = b.length;
+    int i, j, cost, min1, min2, min3;
+    int levenshtein;
+    List<List<int>> d =
+        List.generate(sa + 1, (int i) => List.filled(sb + 1, 0));
+    if (a.isEmpty) {
+      levenshtein = b.length;
+      return (levenshtein);
+    }
+    if (b.isEmpty) {
+      levenshtein = a.length;
+      return (levenshtein);
+    }
+    for (i = 0; i <= sa; i++) {
+      d[i][0] = i;
+    }
+    for (j = 0; j <= sb; j++) {
+      d[0][j] = j;
+    }
+    for (i = 1; i <= a.length; i++) {
+      for (j = 1; j <= b.length; j++) {
+        if (a[i - 1] == b[j - 1]) {
+          cost = 0;
+        } else {
+          cost = 1;
+        }
+        min1 = (d[i - 1][j] + 1);
+        min2 = (d[i][j - 1] + 1);
+        min3 = (d[i - 1][j - 1] + cost);
+        d[i][j] = min(min1, min(min2, min3));
+      }
+    }
+    levenshtein = d[a.length][b.length];
+    return (levenshtein);
   }
 }
